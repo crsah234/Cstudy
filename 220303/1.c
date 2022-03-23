76e3e70a4959cf2038420be75950237ae121e1e6
crsah234 <99774336+crsah234@users.noreply.github.com>
2022-03-23 10:47:47 +0900

    Rename 220309/1.c to 220303/1.c

---
+++ 220303/1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+//## 각 문제마다 프로젝트를 하나씩 추가해야 함!
+
+//2  원주율(3.14)을 매크로상수로 선언하고, 반지름이 6400Km인 지구의 둘레길이를 구하시오.(단, 원주의 크기 = 2 * π * r)
+
+
+int main() {
+	double deposit = 10000000;
+	double after1Y_deposit = deposit * 1.04;
+	double after2Y_deposit = after1Y_deposit * 1.04;
+	printf("2년후에 받는 총액 : %.2lf", after2Y_deposit);
+	return(0);
+	return(0);
+}
+
+
